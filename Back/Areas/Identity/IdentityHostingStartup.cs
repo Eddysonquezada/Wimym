413ec3e7b0e8116233d5039048d52f857d775ee9
Starling Germosen <sgrysoft@outlook.com>
2019-02-17 14:10:52 -0400

    configuration and fisrt controller

---
+++ Back/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Back.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Back.Areas.Identity.IdentityHostingStartup))]
+namespace Back.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ _Implements/__FinalProj/Wimym.DatabaseContext/Config/ShopConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Wimym.DatabaseContext.Config
+{
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+    using Model.Domain;
+
+   
+}
